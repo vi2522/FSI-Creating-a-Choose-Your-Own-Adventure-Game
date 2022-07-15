679c7af85d143e4d2910345d58a04138be9cf01a
Victor Barajas <victorbarajas@Victors-MacBook-Pro.local>
2022-07-14 20:37:19 -0700

    for you to review... and to create your choose your own adventure game

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //Your Code here
+
+let response = window.prompt("Do you go left or right?")
+
+console.log(response)
+
+if(response==="left"){
+    console.log("user submitted left")
+
+    let response2 = window.prompt("Do you up or down?")
+
+    console.log(response2)
+
+    if(response2==="up"){
+        console.log("user submitted up")
+    }
+    else if(response2==="down"){
+        console.log("user submitted down")
+    }
+}
+else if(response==="right"){
+    console.log("user submitted right")
+}
+else{
+    console.log("follow instructions")
+}
