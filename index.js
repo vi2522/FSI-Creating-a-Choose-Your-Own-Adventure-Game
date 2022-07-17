b8f4c24e86dd13d04a1ecfe58492fc7dea4fcb87
Victor Barajas <victorbarajas@Victors-MacBook-Pro.local>
2022-07-16 22:01:41 -0700

    adding questions on index.je

--- index.js
+++ index.js
@@ -17,9 +17,24 @@ if(response==="left"){
     else if(response2==="down"){
         console.log("user submitted down")
     }
+    else{
+        console.log("follow instructions")
+    }
 }
 else if(response==="right"){
     console.log("user submitted right")
+
+    let response3 = window.prompt("Do you clockwise or counterclockwise?")
+
+    if(response3==="clockwise"){
+        console.log("user submitted clockwise")
+    }
+    else if(response3==="counterclockwise"){
+        console.log("user submitted counterclockwise")
+    }
+    else{
+        console.log("follow instructions")
+    }
 }
 else{
     console.log("follow instructions")
